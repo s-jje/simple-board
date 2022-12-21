b4cbca2e2c1e48c3fef6b33e65369e26b8f286b0
sjcho <sshsjhome@gmail.com>
2022-12-21 16:22:36 +0900

    id and password validation with special characters

--- src/main/java/com/project/simpleboard/dto/SignUpRequestDto.java
+++ src/main/java/com/project/simpleboard/dto/SignUpRequestDto.java
@@ -9,10 +9,10 @@ import javax.validation.constraints.Pattern;
 @NoArgsConstructor
 public class SignUpRequestDto {
 
-    @Pattern(regexp = "^[a-z0-9]{4,10}$", message = "아이디는 최소 4자 이상, 10자 이하의 알파벳 소문자(a~z), 숫자(0~9)를 입력하여야 합니다.")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*\\d)[a-z\\d]{4,10}$", message = "아이디는 알파벳 소문자(a~z), 숫자(0~9)를 포함하며 4자 이상, 10자 이하여야 합니다.")
     private String username;
 
-    @Pattern(regexp = "^[a-zA-z0-9]{8,15}$", message = "비밀번호는 최소 8자 이상, 15자 이하의 알파벳 대소문자(a~z, A~Z), 숫자(0~9)를 입력하여야 합니다.")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[(?=.*[^\\w\\s])])[A-Za-z\\d(?=.*[^\\w\\s])]{8,15}$", message = "비밀번호는 알파벳 대소문자(a~z, A~Z), 숫자(0~9), 특수 문자를 포함하며 8자 이상, 15자 이하여야 합니다.")
     private String password;
 
     private boolean admin = false;
