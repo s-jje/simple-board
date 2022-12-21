13b17259960a5bc5f866771f2f2a94d38214d2a0
sjcho <sshsjhome@gmail.com>
2022-12-21 19:45:37 +0900

    refactor

--- src/main/java/com/project/simpleboard/domain/User.java
+++ src/main/java/com/project/simpleboard/domain/User.java
@@ -14,7 +14,6 @@ public class User {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
     @NotNull
     private String username;
 
